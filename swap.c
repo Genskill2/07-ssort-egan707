0fa2005e4ca2b9c53cca195b4e0ed2b334ecbe8e
egan707 <84660860+egan707@users.noreply.github.com>
2021-06-30 22:15:22 +0530

    Update swap.c

---
+++ swap.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <cs50.h>
+#include <math.h>
+#include <ctype.h>
+
+void swap_max(int arr[],int l,int n)
+{
+  int temp,max,index;
+  max=arr[n];
+  index=n;
+  
+  for(int i=n;i<l;i++)
+  {
+    if(max<arr[i])
+    {
+      max=arr[i];
+      index=i;
+    }
+  }
+  temp=arr[n];
+  arr[n]=arr[index];
+  arr[index]=teamp;
+  
+}
+
+void ssort(int arr[],int l)
+{
+  for(int i=0;i<l-1:i++)
+  {
+    swap_max(arr,l,i);
+  }
+}
